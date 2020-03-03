7e740a0b481b1e7e3b8f0092f1b7f0fa7a76cc1f
Anton Potapov <protestandprotect52@gmail.com>
2020-03-04 01:26:22 +0300

    Basic datagram listener

---
+++ Sources/Client/Net/DatagramListner.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QUdpSocket>
+#include <QByteArray>
+
+struct IDataConsumer 
+{
+	virtual bool ValidateData(QByteArray& data) = 0;
+};
+
+class DatagramListener : QObject {
+	Q_OBJECT
+public:
+	DatagramListener(quint16 port, IDataConsumer* consumer);
+
+	void Start();
+	bool IsInProgress() const;
+
+private:
+	void ProcessDatagrams();
+
+private:
+	bool m_is_in_progress = false;
+	IDataConsumer* m_consumer = nullptr;
+	quint16 m_port = 0;
+	QUdpSocket m_socket = {};
+};
