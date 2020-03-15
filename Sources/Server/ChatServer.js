c348e3f1a6ecd9f328d0295ba7ca93665215880d
Anton Potapov <protestandprotect52@gmail.com>
2020-03-15 20:48:30 +0300

    Registration: server part

--- Sources/Server/ChatServer.js
+++ Sources/Server/ChatServer.js
@@ -5,6 +5,8 @@ let local_ip = ''
 
 let message_log = []
 let lastIndex = 0
+let users = []
+let access_codes = []
 
 function main() {
     init()
@@ -73,6 +75,41 @@ function startListen() {
         message_log.push(request.body)
         console.log(request.body)
         response.status(200).end()
+    })
+
+    app.post('/register', (request, response) => {
+        let successCode = 0
+        let alreadyUsedEmailCode = 1
+        let noAccessCode = 2
+
+        let access_code = parseInt(request.body.code)
+        let email = request.body.email
+
+        let is_new_user = users.findIndex((element) => {
+            return element.email == email 
+        }) == -1
+
+        let let_access = access_codes.findIndex((element) => {
+            return element.email == email && element.code == access_code
+        })
+
+        if (!is_new_user) {
+            response.status(200).json({ register_status: alreadyUsedEmailCode })
+            return
+        }
+
+        if (let_access >= 0) {
+            users.push({ email: email, name: request.body.name, password: request.body.password })
+            access_codes.splice(let_access, 1)
+            response.status(200).json({ register_status: successCode })
+        }
+        else {
+            response.status(200).json({ register_status: noAccessCode })
+        }
+    })
+
+    app.post('/login', (request, response) => {
+
     })
 
     app.listen(port)
