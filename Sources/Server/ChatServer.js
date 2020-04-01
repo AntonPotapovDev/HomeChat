1a7ddc077b100fda76a3122717ec084bc40de589
Anton Potapov <protestandprotect52@gmail.com>
2020-04-01 10:53:02 +0300

    Move broadcaster to its own module

--- Sources/Server/ChatServer.js
+++ Sources/Server/ChatServer.js
@@ -111,49 +111,12 @@ class ChatServer {
 }
 
 function main() {
-    init();
     let server = new ChatServer(port);
     server.Start();
-    startBroadcast();
+
+    let IPBroadcaster = require('./IPBroadcaster').IPBroadcaster;
+    let broadcaster = new IPBroadcaster(port, broadcast_address, 500)
+    broadcaster.Start()
 }
 
-function init() {
-    const os = require('os')
-    let ifaces = os.networkInterfaces()
-
-    Object.keys(ifaces).forEach( ifname => {
-        let alias = 0
-
-        ifaces[ifname].forEach( iface => {
-            if ('IPv4' !== iface.family || iface.internal !== false)
-                return
-
-            if (alias >= 1)
-                local_ip = iface.address
-            else
-                local_ip = iface.address
-
-            ++alias
-        })
-    })
-}
-
-function startBroadcast() {
-    const emiter = require('dgram').createSocket('udp4')
-    const interval = 500
-
-    let broadcast = () => {
-        let msg = Buffer.from(local_ip)
-        emiter.send(msg, 0, msg.length, port, broadcast_address, err => {
-            if (err)
-                console.log(err)
-        })
-    }
-
-    emiter.bind(() => {
-        emiter.setBroadcast(true)
-        broadcast_task = setInterval(broadcast, interval)
-    })
-}
-
-main()
+main();
--- Sources/Server/IPBroadcaster.js
+++ Sources/Server/IPBroadcaster.js
@@ -1,1 +1,55 @@
 
+class IPBroadcaster {
+	constructor(port, broadcast_address, broadcast_interval) {
+        this._port = port;
+        this._local_ip = '';
+        this._address = broadcast_address;
+        this._interval = broadcast_interval;
+
+        this._find_local_ip();
+        if (this._local_ip.length == 0)
+            console.log('error!');
+	}
+
+    Start() {
+        const emiter = require('dgram').createSocket('udp4');
+
+        emiter.bind(() => {
+            emiter.setBroadcast(true);
+            broadcast_task = setInterval(this._broadcast, this._interval);
+        });
+    }
+
+    _broadcast() {
+        let msg = Buffer.from(this._local_ip);
+        emiter.send(msg, 0, msg.length, this._port, this._address, error => {
+            if (error)
+                console.log(error);
+        });
+    }
+
+    _find_local_ip() {
+        const os = require('os');
+        let ifaces = os.networkInterfaces();
+
+        Object.keys(ifaces).forEach( ifname => {
+            let alias = 0;
+
+            ifaces[ifname].forEach( iface => {
+                if ('IPv4' !== iface.family || iface.internal !== false)
+                    return;
+
+                if (alias >= 1)
+                    this._local_ip = iface.address;
+                else
+                    this._local_ip = iface.address;
+
+                ++alias;
+            });
+        });
+    }
+}
+
+module.exports = {
+    IPBroadcaster: IPBroadcaster
+};
